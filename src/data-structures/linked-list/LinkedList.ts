b72110dd8ea706dc837d2f80f33e14a025b4a8c6
karkh <karkh@jetstyle.co>
2023-02-02 22:26:03 +0500

    wip linked list

--- src/data-structures/linked-list/LinkedList.ts
+++ src/data-structures/linked-list/LinkedList.ts
@@ -4,28 +4,54 @@ import LinkedListNode from './LinkedListNode';
 export default class LinkedList<T> {
   head: LinkedListNode<T>;
   tail: LinkedListNode<T>;
-  compare: Comparator
+  compare: Comparator<T>
 
-  constructor() {
+  constructor(comparatorFunction?) {
     this.head = null;
     this.tail = null;
-    // this.compare = new Comparator(comparatorFunction);
+    this.compare = new Comparator(comparatorFunction);
   }
 
   append(value: T): LinkedList<T> {
-    const newNode = new LinkedListNode<T>(value);
+    const node = new LinkedListNode<T>(value);
 
     if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
+      this.head = node;
+      this.tail = node;
 
       return this
     }
 
-    this.tail.next = newNode;
-    this.tail = newNode;
+    this.tail.next = node;
+    this.tail = node;
 
     return this;
+  }
+
+  prepend(value): LinkedList<T> {
+    const node = new LinkedListNode<T>(value, this.head)
+    this.head = node
+
+    if (!this.tail) {
+      this.tail = node
+    }
+
+    return this
+  }
+
+  delete(value): LinkedListNode<T> {
+    if (!this.head) {
+      return null
+    }
+
+    let deletedNode = null
+
+    while (this.compare.equal(this.head.value, value)) {
+      deletedNode = this.head;
+      this.head = this.head.next;
+    }
+
+
   }
 
   toArray(): LinkedListNode<T>[] {
