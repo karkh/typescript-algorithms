aa34343906a482917a23dbea76a7ca719d6c987a
karkh <karkh@jetstyle.co>
2023-02-02 22:25:39 +0500

    wip comparator

--- src/utils/comporator/Comparator.ts
+++ src/utils/comporator/Comparator.ts
@@ -1,3 +1,43 @@
-export default class Comparator {
-  //
+type CompareFunction<T> = (a: T, b: T) => number;
+
+export default class Comparator<T> {
+
+  compare: CompareFunction<T>
+  constructor(compareFunction?) {
+    this.compare = compareFunction || Comparator.defaultCompareFunction;
+  }
+
+  static defaultCompareFunction(a, b) {
+    if (a === b) {
+      return 0;
+    }
+
+    return a < b ? -1 : 1;
+  }
+
+  equal(a, b) {
+    return this.compare(a, b) === 0;
+  }
+
+  lessThan(a, b) {
+    return this.compare(a, b) < 0;
+  }
+
+  greaterThan(a, b) {
+    return this.compare(a, b) > 0;
+  }
+
+  lessThanOrEqual(a, b) {
+    return this.lessThan(a, b) || this.equal(a, b);
+  }
+
+  greaterThanOrEqual(a, b) {
+    return this.greaterThan(a, b) || this.equal(a, b);
+  }
+
+  reverse() {
+    const compareOriginal = this.compare;
+    this.compare = (a, b) => compareOriginal(b, a);
+  }
+
 }
